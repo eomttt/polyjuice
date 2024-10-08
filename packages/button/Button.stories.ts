1b6d75987fd4049fb19a55f427a2e0caf0b322a1
eomttt <hyunt0413@gmail.com>
2024-10-08 19:54:28 +0900

    feat. Add slot in button

--- packages/button/Button.stories.ts
+++ packages/button/Button.stories.ts
@@ -1,12 +1,20 @@
-import { Button } from './Button';
-
-customElements.get('polyjuice-button') ||
-  customElements.define('polyjuice-button', Button);
+import './Button';
 
 export default {
   title: 'Polyjuice/Button',
 };
 
-const Template = () => `<polyjuice-button>Button</polyjuice-button>`;
+const Template = () =>
+  `<polyjuice-button>
+    ButtonText
+  </polyjuice-button>`;
+
+const TemplateWithSlot = () =>
+  `<polyjuice-button>
+    <span slot="left-slot">LeftSlot</span>
+    ButtonText
+    <span slot="right-slot">RightSlot</span>
+  </polyjuice-button>`;
 
 export const Default = Template.bind({});
+export const WithSlot = TemplateWithSlot.bind({});
--- packages/button/Button.ts
+++ packages/button/Button.ts
@@ -1,21 +1,36 @@
 class Button extends HTMLElement {
+  private _root: ShadowRoot;
+
   constructor() {
     super();
+    this._root = this.attachShadow({ mode: 'open' });
   }
 
   get leftSlot() {
     return this.getAttribute('left-slot');
   }
 
+  get rightSlot() {
+    return this.getAttribute('right-slot');
+  }
+
   connectedCallback() {
-    this.innerHTML = this.template({
+    this._root.innerHTML = this.template({
       leftSlot: this.leftSlot,
+      rightSlot: this.rightSlot,
     });
   }
 
-  template(state: { leftSlot: any }) {
-    return `<button>${this.innerHTML}</button>`;
+  template(state: { leftSlot: any; rightSlot: any }) {
+    return `
+      <button>
+      <slot name="left-slot"></slot>
+      <slot></slot>
+      <slot name="right-slot"></slot>
+      </button>`;
   }
 }
+
+customElements.define('polyjuice-button', Button);
 
 export { Button };
